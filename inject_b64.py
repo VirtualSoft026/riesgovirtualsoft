f5807dcc850ed0e8a96589ec0f50667397b5afcd
Maria0327-1993 <maria.sanchez@virtualsoft.tech>
2026-07-18 18:54:13 -0500

    Actualizacion automatica de datos - sáb 18/07/2026 18:54:12,90

--- inject_b64.py
+++ inject_b64.py
@@ -1,8 +1,15 @@
 import base64
+import re
+
 with open('assets/src/img/membrete_full.png', 'rb') as f:
     img_data = base64.b64encode(f.read()).decode('utf-8')
+
 with open('index.html', 'r', encoding='utf-8') as f:
     html = f.read()
-html = html.replace('src="assets/src/img/membrete_full.png"', f'src="data:image/png;base64,{img_data}"')
+
+# Use regex to replace the src attribute of printMembreteImg
+new_src = f'src="data:image/png;base64,{img_data}"'
+html = re.sub(r'(<img\s+id="printMembreteImg"\s+)src="[^"]*"', r'\1' + new_src, html)
+
 with open('index.html', 'w', encoding='utf-8') as f:
     f.write(html)
